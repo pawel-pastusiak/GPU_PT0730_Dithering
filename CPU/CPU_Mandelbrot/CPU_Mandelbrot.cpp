217bf578d04a0676c714f20152d648a5f1244e32
Student248833 <248833@student.pwr.edu.pl>
2021-11-16 22:32:38 +0100

    Inicjalizacja części projektu wykonanej w CPU

---
+++ CPU/CPU_Mandelbrot/CPU_Mandelbrot.cpp
@@ -0,0 +1,42 @@
+// CPU_Mandelbrot.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void pointApproximation(float* realPart, float* imagPart, int* maxIter, int* approximation);
+
+int main()
+{
+    float realPoint = 0.2;
+    float imagPoint = 0.2;
+    int max = 100;
+    int appro = 0;
+    pointApproximation(&realPoint, &imagPoint, &max, &appro);
+    cout << appro;
+
+    return 0;
+}
+
+void pointApproximation(float* realPart, float* imagPart, int* maxIter, int* approximation)
+{
+    int i = 0;
+    float zReal = 0;
+    float zImag = 0;
+    float zTempReal = 0;
+    float zTempImag = 0;
+
+    while (i < *maxIter && (zReal * zReal + zImag * zImag < 4))
+    {
+        zTempReal = zReal * zReal - zImag * zImag;
+        zTempImag = 2 * zReal * zImag;
+
+        zReal = zTempReal + *realPart;
+        zImag = zTempImag + *imagPart;
+
+        i++;
+    }
+
+    *approximation = i;
+}
