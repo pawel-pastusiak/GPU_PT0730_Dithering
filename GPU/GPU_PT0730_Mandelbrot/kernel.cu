c535546013940c028bbaf75e0100ea011f68a59a
Kilof99 <nugattex@gmail.com>
2021-11-25 22:30:36 +0100

    Podział obszaru na bloki i wątki - wstęp do przetwarzania równoległego

--- GPU/GPU_PT0730_Mandelbrot/kernel.cu
+++ GPU/GPU_PT0730_Mandelbrot/kernel.cu
@@ -7,9 +7,6 @@
 
 using namespace std;
 
-//__global__ void pointApproximation(float* realPart, float* imagPart, int* maxIter, int* approximation);
-//__global__ void traverse(float* startX, float* startY, float* endX, float* endY, float* step, int* maxIter, int* approximation, float* width);
-
 __device__ void pointApproximation(float* realPart, float* imagPart, int* maxIter, int* approximation)
 {
     *approximation = 0;
@@ -38,26 +35,23 @@ __device__ void traverse(float* startX, float* startY, float endX, float endY, f
 {
     int i = 0;
     float curX, curY;
-    curX = *startX;
-    while (curX < endX) {
+    curY = *startY;
+    while (curY < endY) {
         int j = 0;
-        curY = *startY;
-        while (curY < endY) {
+        curX = *startX;
+        while (curX < endX) {
             pointApproximation(&curX, &curY, maxIter, approximation + (i * (int)(*width / *step + 0.5)) + j++);
-            
-            //cout << endl << i++ << ": " << curX << " x " << curY << ": " << *approximation;
-            curY += *step;
+            curX += *step;
         }
-        curX += *step;
+        curY += *step;
         i++;
     }
 }
 
-__global__ void Func(float* startX, float* startY, int* appro, float* step, int* maxIter, float* width)
+__global__ void Func(float* startX, float* startY, int* appro, float* step, int* maxIter, float* width, int* numberThreads)
 {
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    traverse(startX + i, startY + i, (startX[i] + *width), (startY[i] + *width), step, maxIter, appro + i, width);
-    //pointApproximation(startX + i, startY + i, maxIter, appro + i);
+    int i = blockIdx.x * blockDim.x + blockIdx.y * blockDim.y * 8 + threadIdx.x + threadIdx.y * 8;
+    traverse(startX + i, startY + i, (startX[i] + *width), (startY[i] + *width), step, maxIter, appro + (8*((i/8)*4))+2*(i%8), width);
 }
 
 int main()
@@ -82,6 +76,10 @@ int main()
     int* approximations;
     approximations = (int*)malloc(sizeof(int) * (width/step)* (width / step)* numberThreads * numberThreads);
 
+    for (int i = 0; i < (width / step) * (width / step) * numberThreads * numberThreads; i++) {
+        approximations[i] = -2;
+    }
+
     for (int i = 0; i < numberThreads; i++)
     {
         for (int j = 0; j < numberThreads; j++)
@@ -96,6 +94,7 @@ int main()
     float* width_c;
     int* max_c;
     float* step_c;
+    int* numberThreads_c;
 
     cudaMalloc((void**)&realPoints_c, sizeof(float) * numberThreads*numberThreads);
     cudaMalloc((void**)&imagPoints_c, sizeof(float) * numberThreads * numberThreads);
@@ -103,17 +102,19 @@ int main()
     cudaMalloc((void**)&width_c, sizeof(float));
     cudaMalloc((void**)&max_c, sizeof(int));
     cudaMalloc((void**)&step_c, sizeof(float));
+    cudaMalloc((void**)&numberThreads_c, sizeof(int));
 
     cudaMemcpy(realPoints_c, realPoints, sizeof(float) * numberThreads*numberThreads, cudaMemcpyHostToDevice);
     cudaMemcpy(imagPoints_c, imagPoints, sizeof(float) * numberThreads * numberThreads, cudaMemcpyHostToDevice);
     cudaMemcpy(width_c, &width, sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(max_c, &max, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(step_c, &step, sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(approximations_c, approximations, sizeof(int) * (width / step) * (width / step) * numberThreads * numberThreads, cudaMemcpyHostToDevice);
 
     dim3 threadsPerBlock(4, 4);
-    dim3 numBlocks(numberThreads * numberThreads / threadsPerBlock.x, numberThreads * numberThreads / threadsPerBlock.y);
+    dim3 numBlocks(numberThreads / threadsPerBlock.x, numberThreads / threadsPerBlock.y);
 
-    Func << <numBlocks, threadsPerBlock >> > (realPoints_c, imagPoints_c, approximations_c, step_c, max_c, width_c);
+    Func << <numBlocks, threadsPerBlock >> > (realPoints_c, imagPoints_c, approximations_c, step_c, max_c, width_c, numberThreads_c);
 
     cudaMemcpy(approximations, approximations_c, sizeof(int) * (width / step) * (width / step) * numberThreads * numberThreads, cudaMemcpyDeviceToHost);
 
@@ -121,48 +122,11 @@ int main()
     {
         if (i % (int)((width / step) * numberThreads + 0.5) == 0)
             cout << endl;
-        cout << approximations[i] << "\t";
+        if(approximations[i]>=0)
+            cout << approximations[i] << "\t";
+        else
+            cout << "  \t";
     }
-
-    //cudaMemcpy(realPoints, realPoints_c, sizeof(float) * numberThreads * numberThreads, cudaMemcpyDeviceToHost);
-
-    //for (int i = 0; i < numberThreads * numberThreads; i++)
-    //{
-    //    if (i % numberThreads == 0)
-    //        cout << endl;
-    //    cout << "{" << realPoints[i] << "," << imagPoints[i] << "}\t";
-    //}
-
-    //int* den;
-    //den = (int*)malloc(sizeof(int) * 17);
-    //int* d_a;
-    //cudaMalloc((void**)&d_a, sizeof(int) * 17);
-    //cudaMemcpy(d_a, den, sizeof(int) * 17, cudaMemcpyHostToDevice);
-
-    //Func << <1, 17 >> > (d_a);
-
-    //cudaMemcpy(den, d_a, sizeof(int) * 17, cudaMemcpyDeviceToHost);
-
-    //for (int i = 0; i < 17; i++)
-    //{
-    //    cout << den[i] << " ";
-    //}
-
-    //float realPoint = 0.2;
-    //float imagPoint = 0.2;
-    //int max = 0;
-    //float step = 0;
-    //int appro = 0;
-
-    //cout << "Prosze podac dokladnosc liczby zmiennoprzecinkowej: ";
-    //cin >> step;
-    //cout << "\nProsze podac maksymalna liczbe powtorzen funkcji sprawdzajacej przynaleznosc punktu do zbioru: ";
-    //cin >> max;
-
-    //pointApproximation(&realPoint, &imagPoint, &max, &appro);
-    ////cout << appro;
-    //traverse(&realPoint, &imagPoint, new float(1.0), new float(1.0), &step, &max, &appro);
-
     return 0;
 }
 
